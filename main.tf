d0801c498df49fbdf3d10a9dfd433ddc37ad3db4
takuto25-lab <takuto.engineer.tech@gmail.com>
2026-06-07 23:27:34 +0900

    SGまで　修正版

--- main.tf
+++ main.tf
@@ -91,11 +91,11 @@ resource "aws_route_table" "public"{
 
 # PublicRouteTableをPublicSubnetと紐付ける
 resource "aws_route_table_association" "public_a"{
-  subnet_id = aws.subnet.public_a.id
+  subnet_id = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_c"{
-  subnet_id = aws.subnet.public_c.id
+  subnet_id = aws_subnet.public_c.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -188,7 +188,7 @@ resource "aws_security_group" "alb" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cifr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -196,7 +196,7 @@ resource "aws_security_group" "alb" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cifr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -204,7 +204,7 @@ resource "aws_security_group" "alb" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -232,7 +232,7 @@ resource "aws_security_group" "ecs" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
