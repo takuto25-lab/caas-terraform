6917a956177f7355b4b4efb1614f1fe3dc7de7c2
takuto25-lab <takuto.engineer.tech@gmail.com>
2026-07-05 00:46:20 +0900

    ECR作成

--- main.tf
+++ main.tf
@@ -287,3 +287,40 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# TaskRole作成
+resource "aws_iam_role" "ecs_task_role"{
+  name = "${var.project_name}_ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# ECS Repository作成
+resource "aws_ecr_repository" "main" {
+  name = "${var.project_name}-repository"
+
+  #　タグの上書きを許可
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name ="${var.project_name}_ecr"
+  }
+}
